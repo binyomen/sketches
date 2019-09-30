16257c901cdac08c09fb3792d90f5d8edd85b68a
Ben Weedon <ben@weedon.email>
2019-09-30 13:21:23 -0700

    Add flow field sketch

---
+++ flowField.js
@@ -0,0 +1,130 @@
+// Based on https://www.youtube.com/watch?v=BjoM9oKOAKY
+
+const INCR = 0.1;
+
+const SCALE = 10;
+let COLS;
+let ROWS;
+
+let particles = [];
+let flowField = [];
+
+let fr;
+
+function setup() {
+    createCanvas(1080, 720);
+    pixelDensity(1);
+
+    background(255);
+
+    COLS = floor(width / SCALE);
+    ROWS = floor(height / SCALE);
+
+    for (const i of Array(10000).keys()) {
+        particles[i] = new Particle();
+    }
+
+    flowField = new Array(COLS*ROWS);
+
+    fr = createP('');
+}
+
+class Particle {
+    constructor() {
+        this.pos = createVector(random(width), random(height));
+        this.vel = createVector(0, 0);
+        this.acc = createVector(0, 0);
+        this.maxSpeed = 2;
+        this.prevPos = this.pos.copy();
+    }
+
+    follow(flowField) {
+        const x = floor(this.pos.x / SCALE);
+        const y = floor(this.pos.y / SCALE);
+        const index = x + y * COLS;
+
+        const force = flowField[index];
+        this.applyForce(force);
+    }
+
+    update() {
+        this.prevPos = this.pos.copy();
+
+        this.vel.add(this.acc);
+        this.vel.limit(this.maxSpeed);
+        this.pos.add(this.vel);
+
+        this.acc.mult(0);
+        this.edges();
+    }
+
+    edges() {
+        if (this.pos.x > width) {
+            this.pos.x = 0;
+            this.prevPos = this.pos.copy();
+        }
+        if (this.pos.x < 0) {
+            this.pos.x = width;
+            this.prevPos = this.pos.copy();
+        }
+
+        if (this.pos.y > height) {
+            this.pos.y = 0;
+            this.prevPos = this.pos.copy();
+        }
+        if (this.pos.y < 0) {
+            this.pos.y = height;
+            this.prevPos = this.pos.copy();
+        }
+    }
+
+    applyForce(force) {
+        this.acc.add(force);
+    }
+
+    show() {
+        stroke(0, 5);
+        strokeWeight(1);
+        line(this.prevPos.x, this.prevPos.y, this.pos.x, this.pos.y);
+    }
+}
+
+let zOffset = 0;
+function draw() {
+    let yOffset = 0;
+    for (const y of Array(ROWS).keys()) {
+        let xOffset = 0;
+        for (const x of Array(COLS).keys()) {
+            const index = x + y * COLS;
+
+            const angle = noise(xOffset, yOffset, zOffset) * TWO_PI;
+
+            const v = p5.Vector.fromAngle(angle);
+            v.setMag(2);
+            flowField[index] = v;
+
+            // stroke(0, 50);
+            // strokeWeight(1);
+            // push();
+            // translate(x*SCALE, y*SCALE);
+            // rotate(v.heading());
+            // line(0, 0, SCALE, 0);
+            // pop();
+
+            xOffset += INCR;
+        }
+        yOffset += INCR;
+    }
+
+    for (const particle of particles) {
+        particle.follow(flowField);
+        particle.update();
+        particle.show();
+    }
+
+    zOffset += INCR/5;
+
+    if (frameCount % 5 == 0) {
+        fr.html(floor(frameRate()));
+    }
+}
