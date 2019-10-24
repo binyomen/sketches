f47e08c792216f081719a6705e0ec404d9fdd9f4
Ben Weedon <ben@weedon.email>
2019-10-23 17:50:25 -0700

    Update matrix sketch
    The sketch now follows the actual matrix falling code animation more
    closely. All symbols are placed in a grid, and symbols remain in the
    same location until they are removed.
    
    Some performance improvements can still be made, since the animation is
    jumpy when FPS is low.

--- matrix.js
+++ matrix.js
@@ -1,90 +1,179 @@
-// Based on https://www.youtube.com/watch?v=S1TQCi9axzg
+// Emulating falling code from https://www.youtube.com/watch?v=E6kcwwIQsN4
 
-const SYMBOL_SIZE = 26;
+const TEXT_SIZE = 30;
+const TEXT_OVERFILL = 5;
 
-let streams = [];
+const SPEED_MIN = 10;
+const SPEED_MAX = 100;
+
+const TRAIL_MIN = 8;
+const TRAIL_MAX = 14;
+
+const DECAY_MAX = 50;
+
+let fr;
+let grid;
 
 function setup() {
-    createCanvas(window.innerWidth, window.innerHeight);
+    createCanvas(windowWidth, windowHeight);
 
-    textSize(SYMBOL_SIZE);
+    textSize(TEXT_SIZE + TEXT_OVERFILL);
+    textAlign(LEFT, TOP);
 
-    let x = 0;
-    for (const i of Array(round(width / SYMBOL_SIZE)).keys()) {
-        const y = random(0, -1000);
-        const stream = new Stream(x, y);
-        streams.push(stream);
+    const w = round(width/TEXT_SIZE);
+    const h = round(height/TEXT_SIZE);
+    grid = new Grid(w, h);
 
-        x += SYMBOL_SIZE;
+    for (const x of Array(w).keys()) {
+        grid.spawnDropper(x, round(random(-30, 0)));
+    }
+
+    fr = createP('');
+}
+
+class Dropper {
+    constructor(x, y, grid) {
+        this.x = x;
+        this.y = y;
+        this.grid = grid;
+
+        this.symbols = [];
+        this.time = 0;
+
+        this.speed = random(SPEED_MIN, SPEED_MAX);
+        this.trailLength = round(random(TRAIL_MIN, TRAIL_MAX));
+    }
+
+    update() {
+        this.time += deltaTime;
+
+        if (this.time > this.speed) {
+            this.time = this.time - this.speed;
+
+            const prevSymbol = this.grid.getSymbol(this.x, this.y);
+            if (this.y >= 0 && prevSymbol) {
+                prevSymbol.setBright(false);
+            }
+
+            this.y += 1;
+            if (this.y >= this.grid.getHeight()) {
+                this.y = 0;
+            }
+
+            if (this.y >= 0) {
+                const newSymbol = this.grid.createSymbol(this.x, this.y);
+                newSymbol.setBright(true);
+                this.symbols.push(newSymbol);
+
+                if (this.symbols.length > this.trailLength) {
+                    this.grid.removeSymbol(this.symbols[0].x, this.symbols[0].y);
+                    this.symbols.shift();
+                }
+            }
+        }
     }
 }
 
 class Symbol {
-    constructor(x, y, speed, first) {
+    constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.speed = speed;
-        this.bright = first && (random(0, 1) < 0.5);
+        this.bright = false;
 
-        this.switchInterval = round(random(2, 20));
+        this.setRandomChar();
     }
 
-    setToRandomSymbol() {
-        this.value = String.fromCharCode(0x30a0 + round(random(0, 96)));
+    setRandomChar() {
+        this.char = String.fromCharCode(0x30a0 + round(random(0, 95)));
+    }
+
+    setBright(bright) {
+        this.bright = bright;
+    }
+
+    getCoordinates() {
+        return [this.x*TEXT_SIZE, this.y*TEXT_SIZE];
     }
 
     update() {
-        this.y += this.speed;
-
-        if (this.y > height) {
-            this.y = 0;
-        }
-
-        if ((frameCount - 1) % this.switchInterval == 0) {
-            this.setToRandomSymbol();
+        if (random(0, 1) < 0.001) {
+            this.setRandomChar();
         }
     }
 
     draw() {
+        push();
+
+        // const alpha = map(this.decay, 0, DECAY_MAX, 0, 255);
+        const alpha = 255;
         if (this.bright) {
-            fill(180, 255, 180);
+            fill(180, 255, 180, alpha);
         } else {
-            fill(0, 255, 70);
+            fill(0, 255, 70, alpha);
         }
 
-        text(this.value, this.x, this.y);
+        const [x, y] = this.getCoordinates();
+        text(this.char, x, y);
+
+        pop();
     }
 }
 
-class Stream {
-    constructor(x, y) {
-        this.symbols = [];
-        this.speed = random(5, 10);
+class Grid {
+    constructor(w, h) {
+        this.w = w;
+        this.h = h;
 
-        this.generateSymbols(x, y);
+        this.dict = {};
+        this.droppers = [];
     }
 
-    generateSymbols(x, y) {
-        const SIZE = round(random(5, 30));
-        let first = true;
-        for (let i of Array(SIZE).keys()) {
-            const symbol = new Symbol(x, y, this.speed, first);
-            this.symbols.push(symbol);
-
-            y -= SYMBOL_SIZE;
-            first = false;
+    getSymbol(x, y) {
+        if (x in this.dict && y in this.dict[x]) {
+            return this.dict[x][y];
+        } else {
+            return null;
         }
     }
 
+    createSymbol(x, y) {
+        if (!(x in this.dict)) {
+            this.dict[x] = {};
+        }
+
+        this.dict[x][y] = new Symbol(x, y);
+        return this.dict[x][y];
+    }
+
+    removeSymbol(x, y) {
+        delete this.dict[x][y];
+    }
+
+    getHeight() {
+        return this.h;
+    }
+
+    spawnDropper(x, y) {
+        this.droppers.push(new Dropper(x, y, this));
+    }
+
     update() {
-        for (const symbol of this.symbols) {
-            symbol.update();
+        for (const x in this.dict) {
+            for (const y in this.dict[x]) {
+                this.dict[x][y].update();
+            }
+        }
+
+        for (const dropper of this.droppers) {
+            dropper.update();
         }
     }
 
     draw() {
-        for (const symbol of this.symbols) {
-            symbol.draw();
+        for (const x in this.dict) {
+            for (const y in this.dict[x]) {
+                this.dict[x][y].draw();
+            }
         }
     }
 }
@@ -92,8 +181,10 @@ class Stream {
 function draw() {
     background(0);
 
-    for (const stream of streams) {
-        stream.update();
-        stream.draw();
+    grid.update();
+    grid.draw();
+
+    if (frameCount % 5 == 0) {
+        fr.html('FPS: ' + floor(frameRate()));
     }
 }
