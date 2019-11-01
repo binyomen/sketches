c2f5a80940acaddc5967c91839d479bd15246119
Ben Weedon <ben@weedon.email>
2019-11-01 13:37:43 -0700

    Add support for drawing images to the frame and apply to de_jong_ifs
    This is fairly complex to do, as it requires writing out the image as a
    texture. Also, some DPI weirdness still needs to be resolved. For
    example, on my Windows laptop the window API returns my DPI as 1.25.
    However, to actually scale the image and framebuffer to fill the window
    they need to be scaled up by a factor of 2. This needs more attention to
    see what the correct behavior to follow is.

--- de_jong_ifs/src/main.rs
+++ de_jong_ifs/src/main.rs
@@ -1,8 +1,14 @@
 // Based on https://www.youtube.com/watch?v=vLlbEZt-3j0
 
-use nannou::prelude::*;
+use nannou::{
+    color::FromColor,
+    image::{self, ImageBuffer, RgbaImage},
+    prelude::*,
+};
 
-const SCALE: u32 = 100;
+const SCALE: u32 = 150;
+const WIDTH: u32 = 4 * SCALE;
+const HEIGHT: u32 = 4 * SCALE;
 
 fn main() {
     nannou::app(model).view(view).run();
@@ -13,7 +19,7 @@ struct Model {}
 fn model(app: &App) -> Model {
     app.new_window()
         .with_title("De Jong IFS")
-        .with_dimensions(4 * SCALE, 4 * SCALE)
+        .with_dimensions(WIDTH, HEIGHT)
         .build()
         .unwrap();
     app.set_loop_mode(LoopMode::loop_once());
@@ -22,31 +28,39 @@ fn model(app: &App) -> Model {
 }
 
 fn view(app: &App, _model: &Model, frame: &Frame) {
-    let draw = app.draw();
+    let dpi = 2.0;
 
-    const BACKGROUND_COLOR: f32 = 30.0 / 255.0;
-    draw.background()
-        .rgb(BACKGROUND_COLOR, BACKGROUND_COLOR, BACKGROUND_COLOR);
+    let mut img: RgbaImage = ImageBuffer::from_pixel(
+        (WIDTH as f32 * dpi + 1.0) as u32,
+        (HEIGHT as f32 * dpi + 1.0) as u32,
+        image::Rgba([30, 30, 30, 255]),
+    );
 
     let mut current_point = pt2(0.0, 0.0);
-    for i in 0..99999 {
+    for i in 0..9999999 {
         let color = hsv(
             ((i % 100) as f32 + 80.0) / 255.0,
             100.0 / 255.0,
             150.0 / 255.0,
         );
-        draw.ellipse()
-            .x_y(
-                current_point.x * SCALE as f32,
-                current_point.y * SCALE as f32,
-            )
-            .radius(0.5)
-            .color(color);
+        let color = Rgb::from_hsv(color);
+
+        let x =
+            ((current_point.x * dpi * SCALE as f32) + (WIDTH as f32 * dpi / 2.0)).round() as u32;
+        let y =
+            ((current_point.y * dpi * SCALE as f32) + (HEIGHT as f32 * dpi / 2.0)).round() as u32;
+
+        let r = (255.0 * color.red) as u8;
+        let g = (255.0 * color.green) as u8;
+        let b = (255.0 * color.blue) as u8;
+        let pixel = image::Rgba([r, g, b, 255]);
+
+        img.put_pixel(x, y, pixel);
 
         current_point = ifs(current_point);
     }
 
-    draw.to_frame(app, &frame).unwrap();
+    nannou_utils::draw_image(&img, dpi, &app, &frame);
 }
 
 fn ifs(p: Vector2) -> Vector2 {
---
+++ nannou_utils/src/image.rs
@@ -0,0 +1,170 @@
+// Based on https://github.com/nannou-org/nannou/blob/13835414afcb3853d6567d25bf11ab44c4ec8ce1/examples/vulkan/vk_image.rs
+
+use nannou::{
+    image::{ImageBuffer, Pixel},
+    prelude::*,
+    vk::Format,
+};
+use std::{cell::RefCell, ops::Deref, sync::Arc};
+
+#[derive(Debug, Default, Clone)]
+struct Vertex {
+    position: [f32; 2],
+}
+vk::impl_vertex!(Vertex, position);
+
+pub fn draw_image<P, Container>(img: &ImageBuffer<P, Container>, dpi: f32, app: &App, frame: &Frame)
+where
+    Container: Deref<Target = [<P as Pixel>::Subpixel]>,
+    Container: Clone,
+    P: Pixel + 'static,
+    <P as Pixel>::Subpixel: 'static,
+    <P as Pixel>::Subpixel: Send,
+    <P as Pixel>::Subpixel: Sync,
+    Format: vulkano::format::AcceptsPixels<<P as Pixel>::Subpixel>,
+{
+    let device = app.main_window().swapchain().device().clone();
+
+    let vertex_buffer = vk::CpuAccessibleBuffer::from_iter(
+        device.clone(),
+        vk::BufferUsage::all(),
+        [
+            Vertex {
+                position: [-1.0, -1.0],
+            },
+            Vertex {
+                position: [-1.0, 1.0],
+            },
+            Vertex {
+                position: [1.0, -1.0],
+            },
+            Vertex {
+                position: [1.0, 1.0],
+            },
+        ]
+        .iter()
+        .cloned(),
+    )
+    .unwrap();
+
+    let vertex_shader = vs::Shader::load(device.clone()).unwrap();
+    let fragment_shader = fs::Shader::load(device.clone()).unwrap();
+
+    let render_pass = Arc::new(
+        vk::single_pass_renderpass!(
+            device.clone(),
+            attachments: {
+                color: {
+                    load: Clear,
+                    store: Store,
+                    format: nannou::frame::COLOR_FORMAT,
+                    samples: app.main_window().msaa_samples(),
+                }
+            },
+            pass: {
+                color: [color],
+                depth_stencil: {}
+            }
+        )
+        .unwrap(),
+    );
+
+    // Load the image to GPU memory.
+    let (texture, _tex_future) = {
+        let (width, height) = img.dimensions();
+        let image_data = img.clone().into_raw().clone();
+
+        vk::ImmutableImage::from_iter(
+            image_data.iter().cloned(),
+            vk::image::Dimensions::Dim2d { width, height },
+            vk::Format::R8G8B8A8Srgb,
+            app.main_window().swapchain_queue().clone(),
+        )
+        .unwrap()
+    };
+
+    let sampler = vk::SamplerBuilder::new().build(device.clone()).unwrap();
+
+    let pipeline: Arc<dyn vk::GraphicsPipelineAbstract + Send + Sync> = Arc::new(
+        vk::GraphicsPipeline::start()
+            .vertex_input_single_buffer::<Vertex>()
+            .vertex_shader(vertex_shader.main_entry_point(), ())
+            .triangle_strip()
+            .viewports_dynamic_scissors_irrelevant(1)
+            .fragment_shader(fragment_shader.main_entry_point(), ())
+            .blend_alpha_blending()
+            .render_pass(vk::Subpass::from(render_pass.clone(), 0).unwrap())
+            .build(device.clone())
+            .unwrap(),
+    );
+
+    let descriptor_set = Arc::new(
+        vk::PersistentDescriptorSet::start(pipeline.clone(), 0)
+            .add_sampled_image(texture.clone(), sampler.clone())
+            .unwrap()
+            .build()
+            .unwrap(),
+    );
+
+    let view_fbo = RefCell::new(ViewFbo::default());
+
+    let [w, h] = frame.swapchain_image().dimensions();
+    let viewport = vk::ViewportBuilder::new().build([w as f32 * dpi, h as f32 * dpi]);
+    let dynamic_state = vk::DynamicState::default().viewports(vec![viewport]);
+
+    // Update view_fbo in case of resize.
+    view_fbo
+        .borrow_mut()
+        .update(frame, render_pass.clone(), |builder, img| builder.add(img))
+        .unwrap();
+
+    let clear_values = vec![[0.0, 1.0, 0.0, 1.0].into()];
+
+    frame
+        .add_commands()
+        .begin_render_pass(view_fbo.borrow().expect_inner(), false, clear_values)
+        .unwrap()
+        .draw(
+            pipeline.clone(),
+            &dynamic_state,
+            vec![vertex_buffer.clone()],
+            descriptor_set.clone(),
+            (),
+        )
+        .unwrap()
+        .end_render_pass()
+        .expect("failed to add `end_render_pass` command");
+}
+
+mod vs {
+    nannou::vk::shaders::shader! {
+    ty: "vertex",
+        src: "
+#version 450
+
+layout(location = 0) in vec2 position;
+layout(location = 0) out vec2 tex_coords;
+
+void main() {
+    gl_Position = vec4(position, 0.0, 1.0);
+    tex_coords = position + vec2(1);
+}"
+    }
+}
+
+mod fs {
+    nannou::vk::shaders::shader! {
+    ty: "fragment",
+        src: "
+#version 450
+
+layout(location = 0) in vec2 tex_coords;
+layout(location = 0) out vec4 f_color;
+
+layout(set = 0, binding = 0) uniform sampler2D tex;
+
+void main() {
+    f_color = texture(tex, tex_coords);
+}"
+    }
+}
