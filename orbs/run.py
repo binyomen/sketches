99be567065b6d77d7bdd5e8b596f0e48f0babcd3
Ben Weedon <ben@weedon.email>
2020-08-02 15:41:46 -0700

    Denoise orbs image

--- orbs/run.py
+++ orbs/run.py
@@ -1,11 +1,34 @@
 import bpy
 import random
 
-# Get rid of the initial cube.
-bpy.data.objects.remove(bpy.data.objects['Cube'])
+def clear_scene():
+    bpy.data.objects.remove(bpy.data.objects['Cube'])
+
+def setup_denoising():
+    if bpy.context.scene.render.engine != 'CYCLES':
+        return
+
+    bpy.context.view_layer.cycles.denoising_store_passes = True
+    bpy.context.scene.use_nodes = True
+    bpy.context.scene.node_tree.nodes.clear()
+
+    tree = bpy.context.scene.node_tree
+
+    composite = tree.nodes.new('CompositorNodeComposite')
+    composite.use_alpha = True
+    render_layers = tree.nodes.new('CompositorNodeRLayers')
+    denoise = tree.nodes.new('CompositorNodeDenoise')
+    denoise.use_hdr = True
+
+    tree.links.new(render_layers.outputs['Noisy Image'], denoise.inputs['Image'])
+    tree.links.new(render_layers.outputs['Denoising Normal'], denoise.inputs['Normal'])
+    tree.links.new(render_layers.outputs['Denoising Albedo'], denoise.inputs['Albedo'])
+
+    tree.links.new(denoise.outputs['Image'], composite.inputs['Image'])
 
 def setup_material(mat, color):
     mat.use_nodes = True
+    mat.node_tree.nodes.clear()
 
     bsdf = mat.node_tree.nodes.new('ShaderNodeBsdfPrincipled')
 
@@ -16,8 +39,11 @@ def setup_material(mat, color):
     bsdf.inputs['Sheen'].default_value = 0.5
     bsdf.inputs['Alpha'].default_value = 0.6
 
-    output = mat.node_tree.nodes.get('Material Output')
-    mat.node_tree.links.new(output.inputs[0], bsdf.outputs[0])
+    output = mat.node_tree.nodes.new('ShaderNodeOutputMaterial')
+    mat.node_tree.links.new(bsdf.outputs['BSDF'], output.inputs['Surface'])
+
+clear_scene()
+setup_denoising()
 
 red = bpy.data.materials.new('orb_red')
 setup_material(red, (1, 0, 0, 1))
