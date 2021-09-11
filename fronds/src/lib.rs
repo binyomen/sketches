a20843b94b6c25c59fafdabebd28246998c5763d
Ben Weedon <ben@weedon.email>
2021-09-10 23:08:00 -0700

    Start experimenting with branching in the fronds

--- fronds/src/lib.rs
+++ fronds/src/lib.rs
@@ -1,21 +1,83 @@
-use nannou::draw::Draw;
+use nannou::{draw::Draw, event::Event};
 
 pub const WIDTH: u32 = 800;
 pub const HEIGHT: u32 = 500;
 
 pub struct Frond {
-    x: f32,
+    branches: Vec<Branch>,
 }
 
 impl Frond {
     pub fn new(x: f32) -> Self {
-        Frond { x }
+        Frond {
+            branches: vec![
+                Branch::new(x, x - 5.0),
+                Branch::new(x, x),
+                Branch::new(x, x + 5.0),
+            ],
+        }
+    }
+
+    pub fn event(&mut self, event: &Event) {
+        for branch in &mut self.branches {
+            branch.event(event);
+        }
     }
 
     pub fn view(&self, draw: &Draw) {
+        for branch in &self.branches {
+            branch.view(draw);
+        }
+    }
+}
+
+struct Branch {
+    center: f32,
+    offset: f32,
+    radius: f32,
+    height: f32,
+}
+
+impl Branch {
+    fn new(center: f32, offset: f32) -> Self {
+        Branch {
+            center,
+            offset,
+            radius: 10.0,
+            height: 0.0,
+        }
+    }
+
+    fn event(&mut self, event: &Event) {
+        match event {
+            Event::Update(_) => {
+                self.height += 0.5;
+                self.radius *= 0.999;
+                self.update_offset();
+            }
+            _ => (),
+        }
+    }
+
+    fn view(&self, draw: &Draw) {
+        const WINDOW_BOTTOM: f32 = -((HEIGHT as f32) / 2.0);
         draw.ellipse()
-            .x_y(self.x, -((HEIGHT as f32) / 2.0))
-            .radius(100.0)
+            .x_y(self.center + self.offset, WINDOW_BOTTOM + self.height)
+            .radius(self.radius)
             .rgb(0.3, 0.3, 0.3);
+    }
+
+    fn update_offset(&mut self) {
+        let direction_multiplier = if self.offset < 0.0 {
+            -1.0
+        } else if self.offset > 0.0 {
+            1.0
+        } else {
+            0.0
+        };
+
+        let shifted_height = (self.height / 100.0) - 4.493;
+        let new_offset = (5.0 / shifted_height) * (shifted_height).sin();
+        self.offset = new_offset * direction_multiplier;
     }
 }
--- fronds/src/main.rs
+++ fronds/src/main.rs
@@ -4,6 +4,7 @@ use {
 };
 
 struct Model {
+    num_updates: u64,
     fronds: Vec<Frond>,
 }
 
@@ -19,15 +20,25 @@ fn model(app: &App) -> Model {
         .unwrap();
 
     Model {
+        num_updates: 0,
         fronds: vec![Frond::new(0.0)],
     }
 }
 
-fn event(_app: &App, _model: &mut Model, _event: Event) {}
+fn event(_app: &App, model: &mut Model, event: Event) {
+    model.num_updates += 1;
+
+    for frond in &mut model.fronds {
+        frond.event(&event);
+    }
+}
 
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
-    draw.background().rgb(0.116, 0.116, 0.116);
+
+    if model.num_updates == 0 {
+        draw.background().rgb(0.116, 0.116, 0.116);
+    }
 
     for frond in &model.fronds {
         frond.view(&draw);
