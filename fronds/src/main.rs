1e4f9261e02b4c2a68f1aba773eea05457251d77
Ben Weedon <ben@weedon.email>
2021-09-10 22:01:51 -0700

    Start fronds sketch

---
+++ fronds/src/main.rs
@@ -0,0 +1,15 @@
+use nannou::{app::App, event::Event, frame::Frame};
+
+struct Model;
+
+fn main() {
+    nannou::app(model).event(event).simple_window(view).run();
+}
+
+fn model(_app: &App) -> Model {
+    Model
+}
+
+fn event(_app: &App, _model: &mut Model, _event: Event) {}
+
+fn view(_app: &App, _model: &Model, _frame: Frame) {}
