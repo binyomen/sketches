1102dbe0590d1acdf9b4f9862b7a38268bfb1433
Ben Weedon <ben@weedon.email>
2021-09-15 15:45:29 -0700

    Write out a file

--- skew_grid/src/main.rs
+++ skew_grid/src/main.rs
@@ -5,6 +5,7 @@ use {
     },
     nannou_imageutil::capture::CaptureHelper,
     skew_grid::{Grid, HEIGHT, WIDTH, WIDTH_1},
+    std::{fs, path::PathBuf},
 };
 
 const SIZE_DIVIDEND: u32 = if WIDTH == WIDTH_1 { 1 } else { 0 };
@@ -27,6 +28,8 @@ fn model(app: &App) -> Model {
         number_of_updates: 1,
     });
 
+    fs::create_dir_all(&capture_directory(app)).unwrap();
+
     Model {
         capture_helper: CaptureHelper::from_main_window(app, [WIDTH, HEIGHT]),
     }
@@ -40,8 +43,19 @@ fn view(app: &App, model: &Model, frame: Frame) {
 
     model.capture_helper.render_image(app, &draw);
     model.capture_helper.display_in_window(&frame);
+
+    let path = capture_directory(app)
+        .join("skew_grid_1")
+        .with_extension("png");
+    model.capture_helper.write_to_file(path).unwrap();
 }
 
 fn exit(app: &App, mut model: Model) {
     model.capture_helper.close(app).unwrap();
+}
+
+fn capture_directory(app: &App) -> PathBuf {
+    app.project_path()
+        .expect("Could not locate project path.")
+        .join("outputs")
 }
