2de97d47ed2d3ab873854847db251c0956dc4db9
Ben Weedon <ben@weedon.email>
2021-09-15 15:12:24 -0700

    Draw edges of cells

--- skew_grid/src/lib.rs
+++ skew_grid/src/lib.rs
@@ -1,5 +1,5 @@
 use {
-    nannou::draw::Draw,
+    nannou::{color::rgb, draw::Draw, geom::Vec2},
     rand::{thread_rng, Rng},
 };
 
@@ -22,17 +22,26 @@ const QUARTER_CELL_HEIGHT: f32 = CELL_HEIGHT / 4.0;
 #[derive(Default)]
 pub struct Grid {
     points: Vec<Vec<Point>>,
+    cells: Vec<Vec<Cell>>,
 }
 
 impl Grid {
     pub fn new() -> Self {
         let mut rng = thread_rng();
+        let points = generate_points(&mut rng);
         Grid {
-            points: generate_points(&mut rng),
+            cells: generate_cells(&points),
+            points,
         }
     }
 
     pub fn view(&self, draw: &Draw) {
+        for row in &self.cells {
+            for cell in row {
+                cell.view(draw);
+            }
+        }
+
         for row in &self.points {
             for point in row {
                 point.view(draw);
@@ -46,14 +55,12 @@ fn generate_points(rng: &mut impl Rng) -> Vec<Vec<Point>> {
     for j in 0..point_rows.capacity() {
         let y = (j as f32) * CELL_HEIGHT - (HALF_HEIGHT as f32);
 
-        let mut point_cols = Vec::with_capacity((NUM_CELLS_X + 1) as usize);
-        for i in 0..point_cols.capacity() {
+        let mut point_row = Vec::with_capacity((NUM_CELLS_X + 1) as usize);
+        for i in 0..point_row.capacity() {
             let x = (i as f32) * CELL_WIDTH - (HALF_WIDTH as f32);
 
-            let is_on_edge = i == 0
-                || i == point_cols.capacity() - 1
-                || j == 0
-                || j == point_rows.capacity() - 1;
+            let is_on_edge =
+                i == 0 || i == point_row.capacity() - 1 || j == 0 || j == point_rows.capacity() - 1;
 
             let (skewed_x, skewed_y) = if is_on_edge {
                 (x, y)
@@ -64,18 +71,42 @@ fn generate_points(rng: &mut impl Rng) -> Vec<Vec<Point>> {
                 )
             };
 
-            point_cols.push(Point {
+            point_row.push(Point {
                 x: skewed_x,
                 y: skewed_y,
             });
         }
 
-        point_rows.push(point_cols);
+        debug_assert_eq!(point_row.len(), (NUM_CELLS_X + 1) as usize);
+        point_rows.push(point_row);
     }
 
+    debug_assert_eq!(point_rows.len(), (NUM_CELLS_Y + 1) as usize);
     point_rows
 }
 
+fn generate_cells(points: &[Vec<Point>]) -> Vec<Vec<Cell>> {
+    let mut cell_rows = Vec::with_capacity(NUM_CELLS_Y as usize);
+    for j in 0..cell_rows.capacity() {
+        let mut cell_row = Vec::with_capacity(NUM_CELLS_X as usize);
+        for i in 0..cell_row.capacity() {
+            cell_row.push(Cell {
+                top_left: points[j][i],
+                top_right: points[j][i + 1],
+                bottom_right: points[j + 1][i + 1],
+                bottom_left: points[j + 1][i],
+            });
+        }
+
+        debug_assert_eq!(cell_row.len(), NUM_CELLS_X as usize);
+        cell_rows.push(cell_row);
+    }
+
+    debug_assert_eq!(cell_rows.len(), NUM_CELLS_Y as usize);
+    cell_rows
+}
+
+#[derive(Clone, Copy)]
 struct Point {
     x: f32,
     y: f32,
@@ -85,7 +116,35 @@ impl Point {
     fn view(&self, draw: &Draw) {
         draw.ellipse()
             .x_y(self.x, self.y)
-            .radius(3.0)
+            .radius(5.0)
             .rgb(1.0, 1.0, 1.0);
+    }
+}
+
+impl From<Point> for Vec2 {
+    fn from(point: Point) -> Self {
+        Vec2::new(point.x, point.y)
+    }
+}
+
+struct Cell {
+    top_left: Point,
+    top_right: Point,
+    bottom_right: Point,
+    bottom_left: Point,
+}
+
+impl Cell {
+    fn view(&self, draw: &Draw) {
+        draw.polygon()
+            .no_fill()
+            .stroke_color(rgb(1.0, 1.0, 1.0))
+            .stroke_weight(2.0)
+            .points([
+                self.top_left,
+                self.top_right,
+                self.bottom_right,
+                self.bottom_left,
+            ]);
     }
 }
